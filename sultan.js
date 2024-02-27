f5030e4846bdba90eee5073065a46100a068d908
Da-ace <sultanadegoke8@gmail.com>
2024-02-27 13:40:46 -0800

    fist js commit

---
+++ sultan.js
@@ -0,0 +1,18 @@
+// const symbol = "_";
+// console.log(symbol);
+// const symbolA = "_";
+
+// symbol = prompt("enter your symbol");
+// if ((symbol = symbolA)) console.log("a+b");
+
+let symbol = "+";
+console.log(symbol);
+let symbolB = "-";
+console.log(symbolB);
+
+symbol = prompt("enter your symbol");
+if (symbol === symbolB) {
+  console.log("A-B");
+} else {
+  console.log("A+B");
+}
